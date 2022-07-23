f936c79615484d5a2a2cac3b6dec49fa0179d0de
mhbkamaei <mh.b.kamaei@gmail.com>
2022-07-23 19:05:04 +0430

    awesome theme

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -49,7 +49,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
+beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 beautiful.useless_gap = 10
 beautiful.gap_single_client = true
 
