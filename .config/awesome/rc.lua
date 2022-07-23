64c58b071f9d93a87ef795b47ff039b9cd574d2c
mhbkamaei <mh.b.kamaei@gmail.com>
2022-07-23 23:03:40 +0430

    adding autostart.sh

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -553,3 +553,4 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+awful.spawn.with_shell("~/.config/awesome/autostart.sh")
