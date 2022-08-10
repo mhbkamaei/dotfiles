c33b0f903e6d459816dc17a13b3dce88a7c3bd4f
mhbkamaei <61786642+mhbkamaei@users.noreply.github.com>
2022-08-10 15:30:25 +0430

    Update packinstall.sh

--- packinstall.sh
+++ packinstall.sh
@@ -2,8 +2,12 @@
 
 cp sources.list /etc/apt/
 
-packages="lightdm sudo git neovim sxhkd picom nitrogen zsh xserver-xorg-core xinit slick-greeter"
+packages="stow kitty lightdm sudo git neovim sxhkd picom nitrogen zsh xserver-xorg-core xinit slick-greeter awesome"
 
 apt update
 apt upgrade -y
 apt install $packages -y
+mv /root/dotfiles /home/hbb/
+chown -R hbb:hbb /home/hbb
+stow --verbose --adopt --dir=/home/hbb/dotfiles/ --target=/home/hbb/ .
+chown -R /home/hbb
