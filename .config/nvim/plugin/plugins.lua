b7092ff8ca8764cae7af441eedc4f299f2a1bec4
mhbkamaei <61786642+mhbkamaei@users.noreply.github.com>
2022-08-10 15:47:15 +0430

    Update plugins.lua

--- .config/nvim/plugin/plugins.lua
+++ .config/nvim/plugin/plugins.lua
@@ -1,4 +1,7 @@
 return require('packer').startup(function()
+    
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
