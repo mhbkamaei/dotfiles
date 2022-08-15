7ab4cb7c3bfbb59d6295fff1bbfc731b464c0c1b
mhbkamaei <61786642+mhbkamaei@users.noreply.github.com>
2022-08-15 16:43:00 +0430

    Update packinstall.sh

--- packinstall.sh
+++ packinstall.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-cp sources.list /etc/apt/
-
-packages="unzip gcc stow zsh bash-completion lightdm sudo git sxhkd picom nitrogen zsh xserver-xorg-core xinit slick-greeter awesome"
-
-wget https://github.com/neovim/neovim/releases/download/v0.7.2/nvim-linux64.deb
-apt install ./nvim-linux64.deb
+packages="unzip neovim vim stow zsh lightdm sxhkd picom feh xserver-xorg-core xinit slick-greeter bspwm dmenu"
 
 apt update
 apt upgrade -y
 apt install $packages -y
-mv /root/dotfiles /home/hbb/
-chown -R hbb:hbb /home/hbb
+apt install .
 stow --verbose --adopt --dir=/home/hbb/dotfiles/ --target=/home/hbb/ .
-chown -R /home/hbb
