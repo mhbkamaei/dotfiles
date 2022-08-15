a1d18dc80301e46b938c88b0aa7e059c93b6355a
mhbkamaei <61786642+mhbkamaei@users.noreply.github.com>
2022-08-15 19:08:01 +0430

    Update hbb.sh

--- hbb.sh
+++ hbb.sh
@@ -2,3 +2,4 @@ stow --verbose --adopt --dir=/home/hbb/rising/dotfiles/ --target=/home/hbb/ .
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
