5269ceac76cd19e5e1793f7821a862e2215588af
mhbkamaei <61786642+mhbkamaei@users.noreply.github.com>
2022-08-15 16:54:27 +0430

    Update packinstall.sh

--- packinstall.sh
+++ packinstall.sh
@@ -4,6 +4,6 @@ packages="unzip neovim vim stow zsh lightdm sxhkd picom feh xserver-xorg-core xi
 
 apt update
 apt upgrade -y
-apt install $packages -y
-apt install ./lsd_0.22.0._amd64.deb
+apt install $packages --no-install-recommends --no-install-suggests -y
+apt install ../lsd_0.22.0._amd64.deb
 stow --verbose --adopt --dir=/home/hbb/dotfiles/ --target=/home/hbb/ .
