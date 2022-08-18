8d1360b12249e108bdf386f91ceb6cd200bda004
mhbkamaei <61786642+mhbkamaei@users.noreply.github.com>
2022-08-18 10:37:33 +0430

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 utills="unzip neovim kitty vim stow"
-zsh="zsh zsh-antigen zsh-complition zsh-highlight"
+zsh="zsh zsh-antigen zsh-autosuggestions zsh-syntax-highlighting"
 xorg="xserver-xorg-core xinit lightdm slick-greeter"
 wm="sxhkd picom feh bspwm rofi"
 
@@ -11,4 +11,4 @@ apt install $xorg   --no-install-recommends --no-install-suggests -y
 apt install $zsh    --no-install-recommends --no-install-suggests -y
 apt install $utills --no-install-recommends --no-install-suggests -y
 apt install $wm     --no-install-recommends --no-install-suggests -y
-apt install ../lsd_0.22.0._amd64.deb
+apt install ./lsd_0.22.0_amd64.deb
