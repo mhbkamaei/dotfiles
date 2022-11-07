159d8142d51565147bf6ce73167b9594b08b7dcf
mhbkamaei <61786642+mhbkamaei@users.noreply.github.com>
2022-11-07 14:07:05 -0500

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-tools="growisofs zoxide fzf fd-find gcc npm firefox-esr"
+tools="growisofs zoxide fzf fd-find firefox-esr"
+tree-sitter="gcc g++ libstdc++-12-dev npm"
 utills="unzip neovim kitty stow exa ranger xdotool brightnessctl pip uget aria2"
 zsh="zsh zsh-antigen"
 xorg="xserver-xorg-core xserver-xorg-input-libinput xinit xclip lightdm slick-greeter"
 wm="sxhkd picom feh bspwm dunst polybar"
 
 sudo apt update
+
 sudo apt upgrade -y
-sudo apt install $tools   --no-install-recommends --no-install-suggests -y
-sudo apt install $xorg   --no-install-recommends --no-install-suggests -y
-sudo apt install $zsh    --no-install-recommends --no-install-suggests -y
-sudo apt install $utills --no-install-recommends --no-install-suggests -y
-sudo apt install $wm     --no-install-recommends --no-install-suggests -y
+
+sudo apt install $tools         --no-install-recommends --no-install-suggests -y
+sudo apt install $tree-sitter   --no-install-recommends --no-install-suggests -y
+sudo apt install $xorg          --no-install-recommends --no-install-suggests -y
+sudo apt install $zsh           --no-install-recommends --no-install-suggests -y
+sudo apt install $utills        --no-install-recommends --no-install-suggests -y
+sudo apt install $wm            --no-install-recommends --no-install-suggests -y
+
+mkdir $HOME/.config $HOME/.local/bin -p
+ln -s $(which fdfind) $HOME/.local/bin/fd
 
 chsh hbb -s /bin/zsh
-ln -s $(which fdfind) $HOME/.local/bin/fd
